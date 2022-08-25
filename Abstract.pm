1760cd342c0acf9c32c266f2ce24c14ca08e4a24
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-08-25 16:17:55 +0200

    Simplify code and functionality
    Input versions file is simple txt file with versions per line
    We cannot handle app versions

--- Abstract.pm
+++ Abstract.pm
@@ -6,8 +6,6 @@ use warnings;
 use Class::Utils qw(set_params);
 use English;
 use Error::Pure qw(err);
-use JSON::XS qw(decode_json);
-use List::Util qw(max);
 use Perl6::Slurp qw(slurp);
 
 our $VERSION = 0.01;
@@ -25,38 +23,19 @@ sub new {
 	# Process parameters.
 	set_params($self, @params);
 
-	# Load JSON file with versions.
-	$self->{'_versions_json_file'} = $self->_versions_json_file;
-	my $versions_json = slurp($self->{'_versions_json_file'});
-	$self->{'_versions'} = decode_json($versions_json);
+	# Load file with versions.
+	$self->{'_versions_file'} = $self->_versions_file;
+	my @versions = slurp($self->{'_versions_file'}, {'chomp' => 1});
+	$self->{'_versions'} = \@versions;
 
 	# Set version to last if isn't defined.
 	if (! defined $self->{'version'}) {
-		$self->{'version'} = $self->{'_versions'}->{'versions'}->[-1]->{'version'};
+		$self->{'version'} = $self->{'_versions'}->[-1];
 	}
 
-	# Check version format.
-    	if ($self->{'version'} !~ /^([0-9]+\.[0-9]+\.[0-9]+)$/) {
-		err "Version isn't in proper format.",
-			'Version', $self->{'version'},
-		;
-	};
-
-	# Mapping between version and db schema version.
-	$self->{'_schema_for'} = {
-		map { $_->{'version'} => $_->{'schema'} } @{$self->{'_versions'}->{versions}},
-	};
-
-	# Check schema version.
-	if (! exists $self->{'_schema_for'}->{$self->{'version'}}) {
-		err "Schema version doesn't exist.",
-			'Version', $self->{'version'},
-		;
-	}
-    	$self->{'_schema_version'} = $self->{'_schema_for'}->{$self->{'version'}};
-	if ($self->{'_schema_version'} !~ /^([0-9]+)\.([0-9]+)\.([0-9]+)$/) {
+	if ($self->{'version'} !~ /^([0-9]+)\.([0-9]+)\.([0-9]+)$/) {
 		err 'Schema version has bad format.',
-			'Schema version', $self->{'_schema_version'},
+			'Schema version', $self->{'version'},
 		;
 	}
 
@@ -64,7 +43,7 @@ sub new {
 	$self->{'_schema_module_name'} = $class.'::'."$1\_$2\_$3";
 	eval 'require '.$self->{'_schema_module_name'};
 	if ($EVAL_ERROR) {
-		err 'Cannot load module.',
+		err 'Cannot load Schema module.',
 			'Module name', $self->{'_schema_module_name'},
 		;
 	}
@@ -75,7 +54,7 @@ sub new {
 sub list_versions {
 	my $self = shift;
 
-	return sort map { $_->{'version'} } @{$self->{'_versions'}->{'versions'}};
+	return sort @{$self->{'_versions'}};
 }
 
 sub schema {
@@ -84,22 +63,16 @@ sub schema {
 	return $self->{'_schema_module_name'};
 }
 
-sub schema_version {
-	my $self = shift;
-
-	return $self->{'_schema_version'};
-}
-
 sub version {
 	my $self = shift;
 
 	return $self->{'version'};
 }
 
-sub _versions_json_file {
+sub _versions_file {
 	my $self = shift;
 
-	err "We need to implement distribution JSON file with Schema versions.";
+	err "We need to implement distribution file with Schema versions.";
 
 	return;
 }
