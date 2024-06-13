2a8da2d0b901877665c5a5f8261acbca14666f9e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-06-13 20:06:10 +0200

    Prepare for 0.06 version

--- Abstract.pm
+++ Abstract.pm
@@ -8,7 +8,7 @@ use English;
 use Error::Pure qw(err);
 use Perl6::Slurp qw(slurp);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -268,6 +268,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
